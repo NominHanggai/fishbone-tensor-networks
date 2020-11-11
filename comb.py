40f4a3776a533aeb337c442bdbb527d967f98a08
kavat.lux@gmail.com <kavat.lux@gmail.com>
2020-11-11 00:57:34 -0500

    Comb-like lists to store Hamiltonians and singular values

--- comb.py
+++ comb.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 
 class SimpleTTPS:
@@ -15,7 +16,7 @@ class SimpleTTPS:
                          ⋮ 
     """
 
-    def __init__(self, B=None, S=None,  # ele_list, vib_list, e_bath_list, v_bath_list
+    def __init__(self, B, S,  # ele_list, vib_list, e_bath_list, v_bath_list
                  ):
         # self.e = ele_list
         # self.v = vib_list
@@ -23,23 +24,53 @@ class SimpleTTPS:
         # self.vb = v_bath_list
         (eb, ev, vb) = B
         (eb_s, ev_s, vb_s) = S
-        assert len(eb) == len(ev) == len(vb)
+
+        try:
+            assert len(eb) == len(ev) == len(vb)
+        except ValueError:
+            print >> sys.stderr, "Lengths of the tensor lists don't " \
+                                 "Look back the diagram in the comment " \
+                                 "of the class SimpleTTPS. The tensors " \
+                                 "in the lists should compose a comb-like " \
+                                 "network."
+            sys.exit(1)
+
         self._nc = len(ev)
-        self._eL = [len(ev[i]) for i in range(self._nc)]
-        self._ebL = [len(eb[i]) for i in range(self._nc)]
-        self._vbL = [len(vb[i]) for i in range(self._nc)]
+        self._evL = [len(ev[n]) for n in range(self._nc)]
+        self._ebL = [len(eb[n]) for n in range(self._nc)]
+        self._vbL = [len(vb[n]) for n in range(self._nc)]
+        self._L = [sum(x) for x in zip(self._ebL, self._evL, self._vbL)]
 
         self.ttnB = []
         # ttn means tree tensor network. The self.ttn array stores the tensors in the whole network.
-        for i in range(len(self._eL)):
+        for i in range(self._nc):
             self.ttnB.append(
                 eb[i] + ev[i] + vb[i]
             )
-
+        self._pD = []
+        for n in range(self._nc):
+            self._pD.append(
+                [self.ttnB[n][i].shape[1] for i in range(self._L[i])]
+            )
         self.ttnS = []
-        for i in range(len(self._eL)):
-            self.ttnS.append(
-                eb_s[i] + ev_s[i] + vb_s[i]
+
+        """ ttnS = [ [ S_11, S_12, ..., S_1M, S_backbone_1 ],
+                     [ S_21, S_22, ..., S_2N, S_backbone_2 ],
+                     ...
+                   ]
+        """
+        for n in range(self._nc):
+            self.ttnS.append(eb_s[n] + ev_s[n] + vb_s[n])
+
+        self.ttnH = []
+        """ ttnH = [ [ H_11, H_12, ..., H_1M, H_backbone_1 ],
+                     [ H_21, H_22, ..., H_2N, H_backbone_2 ],
+                     ...
+                   ]
+        """
+        for n in range(self._nc):
+            self.ttnH.append(
+                [np.empty(0, dtype=np.float) for i in range(self._L[n])]
             )
 
     def get_theta1(self, n, i):
@@ -59,18 +90,17 @@ class SimpleTTPS:
 
     def get_theta2(self, n, i):
         """Calculate effective two-site wave function on sites i,j=(i+1) in mixed canonical form.
-
-        The returned array has legs ``vL, i, j, vR``.
-        tuple: the tuple that stores the bond matrix. For example, 
-               the E0--V0 bond is labeled as (0,0),
-               the E0--E1 bond is (0,1),
-               the E1--V1 bond is (1,0)
-               the V1--b11 bond is (1,1), 
-               the E1--d11 bond is (1,-1), etc.
         """
         # n=-1 means the backbone chain. When n=-1, i is the bond number bottom up
         if n == -1:
-            assert 0 <= i <= self._nc - 1
+            try:
+                assert 0 <= i < self._nc - 1
+            except AssertionError:
+                print("The bond is out of the range.",
+                      file=sys.stderr
+                      )
+                raise
+                sys.exit(1)
             upward_b = np.tensordot(
                 self.get_theta1(i, self._ebL[i]),
                 np.diag(self.ttnS[i][self._ebL[i]] ** (-1))
@@ -78,7 +108,7 @@ class SimpleTTPS:
             return np.tensordot(
                 upward_b,
                 self.get_theta1(i + 1, self._ebL[i + 1]),
-                [4, 4]
+                [2, 4]
             )  # vL i [vU] vD vR , vL' j vU' [vD'] vR' -> {vL i VD vR; VL' j vU' vR'}
 
         if n != -1:
@@ -87,6 +117,22 @@ class SimpleTTPS:
             return np.tensordot(
                 self.get_theta1(n, i), self.ttnB[n][i + 1], axes=1
             )  # vL i _vU_ _vD_ [vR],  [vL] j _vU_ _vD_ vR -> {vL i _vU_ _vD_; j _vU_ _vD_ vR}
+
+    def split_truncate_theta(self, theta, n, i):
+        """
+        TODO
+        Split the contracted two-site wave function and truncate the number of singular values.
+        :param theta: the contracted two-site wave function
+        :param n: which chain
+        :param i: which bond on the chain
+        :return: B_1, S, B_2
+        """
+        pass
+
+    def update_bond(self):
+        """TODO
+        """
+        pass
 
 
 def init_ttn(nc, L, d1, d2):
@@ -116,16 +162,48 @@ def init_ttn(nc, L, d1, d2):
     vb_s = np.ones([1], np.float)
     vb_ss = [vb_s.copy() for i in range(L)]
     vb_sss = [vb_ss.copy() for i in range(nc)]
-    print(ebss[1])
-    print(evss[1])
-    print(vbss[1])
+    # print(ebss[1])
+    # print(evss[1])
+    # print(vbss[1])
     return SimpleTTPS(
         (ebss, evss, vbss),
         (eb_sss, ev_sss, vb_sss)
     )
 
 
+def split_truncate_theta(theta, chi_max, eps):
+    """Split and truncate a two-site wave function theta in mixed canonical form.
+
+        Split a two-site wave function as follows::
+              vL --(theta)-- vR     =>    vL --(A)--diag(S)--(B)-- vR
+                    |   |                       |             |
+                    i   j                       i             j
+
+        Afterwards, truncate in the new leg (labeled ``vC``).
+
+        Parameters
+        ----------
+        theta : np.Array[ndim=?:TODO ]
+            Two-site wave function in mixed canonical form, with legs ``vL, i, j, vR``.
+        chi_max : int
+            Maximum number of singular values to keep
+        eps : float
+            Discard any singular values smaller than that.
+
+        Returns
+        -------
+        A : np.Array[ndim=3]
+            Left-canonical matrix on site i, with legs ``vL, i, vC``
+        S : np.Array[ndim=1]
+            Singular/Schmidt values.
+        B : np.Array[ndim=3]
+            Right-canonical matrix on site j, with legs ``vC, j, vR``
+        """
+
+    chivL, dL, dR, chivR = theta.shape
+    theta = np.reshape(theta, [chivL * dL, dR * chivR])
+
+
 if __name__ == "__main__":
     ttn = init_ttn(nc=2, L=3, d1=5, d2=6)
-    ttn.get_theta2(0, 1)
-
+    ttn.get_theta2(-1, 0)
