6699549a34f36f4861292eb531f75a52c8aba5b1
kavat.lux@gmail.com <kavat.lux@gmail.com>
2021-06-22 16:50:18 -0400

    MultiChannel, with positive and negtive couplings

--- fishbonett/backwardSpinBosonMultiChannel.py
+++ fishbonett/backwardSpinBosonMultiChannel.py
@@ -215,13 +215,15 @@ class SpinBoson:
         coef = self.coef
         e = self.phase_func
         mat_list = self.coup_mat_np
+        print(mat_list.shape)
         if star is False:
             phase_factor = np.array([e(w, t) for w in freq])
             print("Geting d's")
             d_nt_mat = [einsum('kst,k,k', mat_list, coef[:, n], phase_factor) for n in range(len(freq))]
             return np.array([mat[0,0] for mat in d_nt_mat])
         else:
-            return coef
+            reorg = sum([mat_list[i, 0, 0] ** 2 / freq[i] for i in range(len(freq))])
+            return reorg
 
     def get_h2(self, t, delta, inc_sys=True):
         print("Geting h2")
