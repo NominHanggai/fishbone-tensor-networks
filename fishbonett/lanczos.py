a46a88415696d7473b609892f6c62de49469fdfd
Mulliken <kavat.lux@gmail.com>
2021-08-24 11:52:55 -0400

    Test Lanczos

--- fishbonett/lanczos.py
+++ fishbonett/lanczos.py
@@ -8,28 +8,30 @@
 import numpy as np
 from math import fsum
 
-def lanczos(A,p):
-    q = np.array(p).copy()
+
+def lanczos(A, p):
+    A = np.array(A, dtype=np.longdouble)
+    q = np.array(p, dtype=np.longdouble).copy()
     n = k = A.shape[0]
-    Q = np.zeros((n,k+1))
-    Q[:,0] = q/np.linalg.norm(q)
+    Q = np.zeros((n, k + 1),dtype=np.longdouble)
+    Q[:, 0] = q / np.linalg.norm(q)
     # print(Q[:,0])
     alpha = 0
     beta = 0
 
     for i in range(k):
-      if i == 0:
-        q = np.dot(A,Q[:,i])
-      else:
-        q = np.dot(A, Q[:,i]) - beta*Q[:,i-1]
-      alpha = np.dot(q.T, Q[:,i])
-      q = q - Q[:,i]*alpha
-      q = q - np.dot(Q[:,:i], np.dot(Q[:,:i].T, q)) # full reorthogonalization
-      beta = np.linalg.norm(q)
-      Q[:,i+1] = q/beta
-      # print(beta)
+        if i == 0:
+            q = np.dot(A, Q[:, i])
+        else:
+            q = np.dot(A, Q[:, i]) - beta * Q[:, i - 1]
+        alpha = np.dot(q.T, Q[:, i])
+        q = q - Q[:, i] * alpha
+        q = q - np.dot(Q[:, :i], np.dot(Q[:, :i].T, q))  # full re-orthogonalization
+        beta = np.linalg.norm(q)
+        Q[:, i + 1] = q / beta
+        # print(beta)
 
-    Q = Q[:,:k]
+    Q = Q[:, :k]
 
     Sigma = np.dot(Q.T, np.dot(A, Q))
     return Sigma, Q
@@ -37,12 +39,11 @@ def lanczos(A,p):
 
 if __name__ == "__main__":
     dim = 1000
-    ele = np.random.rand(dim)*1000
+    ele = np.random.rand(dim) * 1000
     ele.sort()
     mat = np.diag(ele)
-    v0 = np.random.rand(dim)*1
+    v0 = np.random.rand(dim) * 1
     T, Q = lanczos(mat, v0)
     print(np.sort(np.linalg.eigvals(T)) - ele
           )
-    print(Q.T@Q-np.eye(Q.shape[0]))
-
+    print(Q.T @ Q - np.eye(Q.shape[0]))
