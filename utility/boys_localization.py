31e11c8f4d33d4989ceb4d35c1a92278002bc8ab
kavat.lux@gmail.com <kavat.lux@gmail.com>
2022-05-12 19:35:33 -0400

    Fermi's golden rule rate

--- utility/boys_localization.py
+++ utility/boys_localization.py
@@ -9,40 +9,48 @@ Jacobi sweeps are used to find maximum of the Boys function.
 """
 
 s3_mu = [2.2471, 0.2322, -0.3287]
-s2_mu = [27.1776,0.2494,-1.4186]
-s1_mu = [28.7418,0.0702,-1.3821]
-s3s2_mu = np.array([-1.42466459, -0.11795590, 0.10055223])*2.5412
-s3s1_mu = np.array([0.22303506,0.40015346,-0.17319997])*2.5412
-s2s1_mu = np.array([0.15068858,-2.56353252,0.98092125])*2.5412
+s2_mu = [27.1776, 0.2494, -1.4186]
+s1_mu = [28.7418, 0.0702, -1.3821]
+s3s2_mu = np.array([-1.42466459, -0.11795590, 0.10055223]) * 2.5412
+s3s1_mu = np.array([0.22303506, 0.40015346, -0.17319997]) * 2.5412
+s2s1_mu = np.array([0.15068858, -2.56353252, 0.98092125]) * 2.5412
 # Above are dipole moments at the LE geometry. 2.5412 Debye = 1 a.u. diople
-s3s2_elem = np.zeros([3,3]); s3s2_elem[0, 1]=1; s3s2_elem=s3s2_elem+s3s2_elem.T
-s3s1_elem = np.zeros([3,3]); s3s1_elem[0, 2]=1; s3s1_elem=s3s1_elem+s3s1_elem.T
-s2s1_elem = np.zeros([3,3]); s2s1_elem[1, 2]=1; s2s1_elem=s2s1_elem+s2s1_elem.T
-s3_elem = np.zeros([3,3]); s3_elem[0, 0]=1
-s2_elem = np.zeros([3,3]); s2_elem[1, 1]=1
-s1_elem = np.zeros([3,3]); s1_elem[2, 2]=1
+s3s2_elem = np.zeros([3, 3]);
+s3s2_elem[0, 1] = 1;
+s3s2_elem = s3s2_elem + s3s2_elem.T
+s3s1_elem = np.zeros([3, 3]);
+s3s1_elem[0, 2] = 1;
+s3s1_elem = s3s1_elem + s3s1_elem.T
+s2s1_elem = np.zeros([3, 3]);
+s2s1_elem[1, 2] = 1;
+s2s1_elem = s2s1_elem + s2s1_elem.T
+s3_elem = np.zeros([3, 3]);
+s3_elem[0, 0] = 1
+s2_elem = np.zeros([3, 3]);
+s2_elem[1, 1] = 1
+s1_elem = np.zeros([3, 3]);
+s1_elem[2, 2] = 1
 
 mu_mat_3 = np.kron(s3s2_elem, s3s2_mu) + np.kron(s3s1_elem, s3s1_mu) + np.kron(s2s1_elem, s2s1_mu) + \
-         np.kron(s3_elem, s3_mu) + np.kron(s2_elem, s2_mu) + np.kron(s1_elem, s1_mu)
+           np.kron(s3_elem, s3_mu) + np.kron(s2_elem, s2_mu) + np.kron(s1_elem, s1_mu)
 
-mu_mat_3 = mu_mat_3.reshape(3,3,3)
+mu_mat_3 = mu_mat_3.reshape(3, 3, 3)
 
 test_mat_3 = np.array(mu_mat_3)
 
 mu_mat_2 = np.array([
-    [[0.0958,-0.7722,0.1177],[1.238206485004, 0.22711212639999998, -0.08586836777599999]],
-    [[1.238206485004, 0.22711212639999998, -0.08586836777599999], [-27.6663,-5.9275,1.2562]]
+    [[0.0958, -0.7722, 0.1177], [1.238206485004, 0.22711212639999998, -0.08586836777599999]],
+    [[1.238206485004, 0.22711212639999998, -0.08586836777599999], [-27.6663, -5.9275, 1.2562]]
 ])
 
 mu_mat_2_config1 = np.array([
-    [[-1.1410,-0.9247,-0.6088], [-2.84510477626, -0.715646213116, -0.409709899928]],
+    [[-1.1410, -0.9247, -0.6088], [-2.84510477626, -0.715646213116, -0.409709899928]],
     [[-2.84510477626, -0.715646213116, -0.409709899928], [-25.3102, -6.2400, -3.7840]]
 ])
+
+
 # print(mu_mat[:,:,0])
 # print(s2s1_elem, s3s2_elem, s3s1_elem)
-
-
-
 
 
 def boys_func(mat_mu):
@@ -54,10 +62,9 @@ def boys_func(mat_mu):
         i = p[0]
         j = p[1]
         boys_l.append(
-            norm(mat_mu[i,i,:] - mat_mu[j,j,:])**2
+            norm(mat_mu[i, i, :] - mat_mu[j, j, :]) ** 2
         )
     return sum(boys_l)
-
 
 
 def boys_loc(mat_mu, u_final):
@@ -72,46 +79,47 @@ def boys_loc(mat_mu, u_final):
         i = p[0]
         j = p[1]
 
-        mu_ij = mat_mu_after[i,j]
-        mu_ii = mat_mu_after[i,i]
-        mu_jj = mat_mu_after[j,j]
-        F = norm(mu_ij)**2 - .25*norm(mu_ii-mu_jj)**2
-        G = mu_ij@(mu_ii-mu_jj)
-        theta1 = np.arccos(-F/np.sqrt(F**2+G**2))
-        theta2 = np.arcsin(G/np.sqrt(F**2+G**2))
+        mu_ij = mat_mu_after[i, j]
+        mu_ii = mat_mu_after[i, i]
+        mu_jj = mat_mu_after[j, j]
+        F = norm(mu_ij) ** 2 - .25 * norm(mu_ii - mu_jj) ** 2
+        G = mu_ij @ (mu_ii - mu_jj)
+        theta1 = np.arccos(-F / np.sqrt(F ** 2 + G ** 2))
+        theta2 = np.arcsin(G / np.sqrt(F ** 2 + G ** 2))
         # See Joseph's paper for the theory.
-        t1_l = [theta1 + 2*k*np.pi for k in range(-2,3)] + [2*k*np.pi -theta1 for k in range(-1,1)]
-        t2_l = [theta2 + 2*k*np.pi for k in range(-2,3)] + [(2*k+1)*np.pi - theta2 for k in range(-1,1)]
+        t1_l = [theta1 + 2 * k * np.pi for k in range(-2, 3)] + [2 * k * np.pi - theta1 for k in range(-1, 1)]
+        t2_l = [theta2 + 2 * k * np.pi for k in range(-2, 3)] + [(2 * k + 1) * np.pi - theta2 for k in range(-1, 1)]
         # print("theta 1", theta1, np.sort(t1_l))
         # print("theta 2", theta2, np.sort(t2_l))
         # print(-F/np.sqrt(F**2+G**2), [np.cos(x) for x in t1_l])
         # print(G / np.sqrt(F ** 2 + G ** 2), [np.sin(x) for x in t2_l])
-        val_l = [i for i, j in it.product(t1_l, t2_l) if np.abs(i-j) <=0.0001]
+        val_l = [i for i, j in it.product(t1_l, t2_l) if np.abs(i - j) <= 0.0001]
         key = np.argmin(np.abs(val_l))
-        theta = .25*val_l[key]
+        theta = .25 * val_l[key]
         # print(val_l[key])
         u = np.eye(dim)
         u[i, j] = np.sin(theta)
         u[j, i] = -np.sin(theta)
         u[i, i] = u[j, j] = np.cos(theta)
         mat_mu_after = np.einsum('ij,jkX,kl->ilX', u, mat_mu_after, u.T)
-        u_final = u@u_final
+        u_final = u @ u_final
     boys_value = boys_func(mat_mu_after)
-    return u_final,mat_mu_after,boys_value, boys_value_0
+    return u_final, mat_mu_after, boys_value, boys_value_0
+
 
 u_final = np.eye(2)
-boys_value =0; boys_value_0 = 1;
-while(np.abs(boys_value - boys_value_0)>0.001):
+boys_value = 0;
+boys_value_0 = 1;
+while (np.abs(boys_value - boys_value_0) > 0.001):
     u_final, mu_mat_2_config1, boys_value, boys_value_0 = boys_loc(mu_mat_2_config1, u_final)
     print(boys_value, boys_value_0)
 
-
 u = u_final
 H = np.diag([0, -0.1331, -0.2734])
-H = np.diag([0,  -0.0964])
+H = np.diag([0, -0.0964])
 H = np.diag([0, -0.1288])
-print(u@H@u.T)
-print(repr(u@H@u.T*8065.54429))
+print(u @ H @ u.T)
+print(repr(u @ H @ u.T * 8065.54429))
 
 # print(u@test_mat_3[:,:,2]@u.T)
 # print(test_mat_3[:,:,0])
--- utility/golden_rule_rate.py
+++ utility/golden_rule_rate.py
@@ -6,13 +6,14 @@ from legendre_discretization import get_vn_squared, get_approx_func
 Calculate fermi's golden rule rate for electron transfer reactions, given a spectral density.
 """
 
+
 def fgr_rate(c, e, kbT, _w, _v_sq):
     w = np.array(_w)
     v_sq = np.array(_v_sq)
     j_factor = (-v_sq / np.pi / w ** 2)
     coth = 1 / np.tanh(w / (2 * kbT))
-    exponent = lambda t: np.sum(j_factor * (coth * (1 - np.cos(w * t)) - -1j * np.sin(t * w)))
-    integrand = lambda t: np.real(np.exp(exponent(t)) * np.exp(1j * e * t))
+    exponent = lambda t: np.sum(j_factor * (coth * (1 - np.cos(w * t)) - 1j * np.sin(t * w)))
+    integrand = lambda t: np.real(np.exp(exponent(t)) * np.exp(-1j * e * t))
     integral, _ = integrate.quad(integrand, 0, np.inf, limit=5000)
     return 2 * (c ** 2) * integral
 
@@ -23,56 +24,44 @@ def marcus_rate(c: float, e: float, kbT: float, reorg_e: float):
 
 
 if __name__ == "__main__":
+    """
+    Example calculation. Reproduce Figure 2 in dx.doi.org/10.1021/jp400462f | J. Phys. Chem. A 2013, 117, 6196−6204
+    """
+
     import matplotlib.pyplot as plt
 
-    # Lorentzian spectral density
-    reorg_e = 3000
-    Omega = 200
-    Omega2 = 400
-    kbT = 0.69 * 300
-    eta = 200
-    domain = [0, 3000]
-    C_DA = 500
+    # Lorentzian spectral density parameters. Atomic units.
+    reorg_e = 2.39e-2
+    Omega = 3.5e-4
+    kbT = 9.5e-4
+    eta = 1.2e-3
+    domain = [0, 5e-3]
+    C_DA = 5e-5
     j = lambda w: 0.5 * (4 * reorg_e) * Omega ** 2 * eta * w / ((Omega ** 2 - w ** 2) ** 2 + eta ** 2 * w ** 2)
-    # j = lambda w: 0.5 * (4 * reorg_e/2) * Omega ** 2 * eta * w / ((Omega ** 2 - w ** 2) ** 2 + eta ** 2 * w ** 2) \
-    # + 0.5 * (4 * reorg_e/2) * Omega2 ** 2 * eta * w / ((Omega2 ** 2 - w ** 2) ** 2 + eta ** 2 * w ** 2)
 
     w, v_sq = get_vn_squared(j, 1000, domain)
-    print("Discretized Reorganization E", np.sum(v_sq / w / np.pi))
+    print("Discrete Reorganization E", np.sum(v_sq / w / np.pi))
 
     x = np.linspace(*domain, 1000)
     plt.plot(x, j(x), label='j')
-    plt.savefig('j.png')
+    plt.savefig('spectral_density.png')
     plt.clf()
 
-    temp = np.linspace(5, 600, 20)
-    e = np.linspace(4167, 4333, 10)
-    kb = 0.69
+    e = np.linspace(0.015, 0.03, 20)
+    rate_fgr = np.vectorize(lambda ei: fgr_rate(C_DA, ei, kbT, w, v_sq)
+                            )(e)
+    rate_marcus = np.vectorize(lambda ei: marcus_rate(C_DA, ei, kbT, reorg_e))(e)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(e, rate_fgr, 'o-', label='FGR rate')
+    ax.plot(e, rate_marcus, 'x', label='Marcus rate')
+    ax.legend()
+    ax.set_xlabel('E (a.u.)')
+    ax.set_ylabel('Rate (a.u.)')
+    ax.set_xlim(0.015, 0.03)
 
-    for ei in e:
-        r1_fgr = np.array([fgr_rate(C_DA, ei, kb * Ti, w, v_sq) for Ti in temp])
-        r2_fgr = np.array([fgr_rate(np.sqrt(2) * C_DA, ei, kb * Ti, w, v_sq * 2) for Ti in temp])
-        plt.plot(temp, r2_fgr / r1_fgr, label=f'$\Delta G$={ei: .0f}')
+    x_left, x_right = ax.get_xlim()
+    y_low, y_high = ax.get_ylim()
+    ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * 0.4)
 
-    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-    plt.savefig('fgr_ratio.png', bbox_inches="tight")
-    exit()
-    r1_marcus = np.array([marcus_rate(C_DA, ei, kbT, reorg_e) for ei in e])
-    r2_marcus = np.array([marcus_rate(np.sqrt(2) * C_DA, ei, kbT, reorg_e * 2) for ei in e])
-    plt.plot(e, r1_fgr, label='1 Acceptor FGR')
-    plt.plot(e, r2_fgr, label='2 Acceptor FGR')
-    plt.plot(e, r1_marcus, label='1 Acceptor Marcus')
-    plt.plot(e, r2_marcus, label='2 Acceptor Marcus')
-    plt.legend()
-    plt.savefig('rate.png')
-    plt.clf()
-
-    # Ratio
-    plt.plot(e, r2_fgr / r1_fgr, label='Ratio FGR')
-    plt.legend()
-    plt.savefig('rate_ratio_fgr.png')
-    plt.clf()
-    plt.plot(e, r2_marcus / r1_marcus, label='Ratio Marcus')
-    plt.legend()
-    plt.savefig('rate_ratio_marcus.png')
-    plt.clf()
+    fig.savefig('Figure_2.png')
